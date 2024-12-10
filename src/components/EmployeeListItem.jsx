739c3533dfd6a989dda7c8d7cfd510fa78c65cc2
Chris3467 <chris.collado815@gmail.com>
2024-12-10 08:47:59 -0500

    Finished lab

---
+++ src/components/EmployeeListItem.jsx
@@ -0,0 +1,11 @@
+function EmployeeListItem() {
+  return (
+    <div className="employee">
+      <h4>Employee Name</h4>
+      <p>Job Title: Software Engineer</p>
+      <p>Years of Experience: 5</p>
+    </div>
+  );
+}
+
+export default EmployeeListItem;
